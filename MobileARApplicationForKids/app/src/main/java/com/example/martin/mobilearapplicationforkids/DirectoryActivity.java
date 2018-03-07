e42bee17d9159259d452f02b13941b0c1e2dccc6
tuongmartin <tuongbkav@gmail.com>
2018-03-07 16:28:06 +0700

    Implement UI home screen, navigate to another screen, set rollback button

--- MobileARApplicationForKids/app/src/main/java/com/example/martin/mobilearapplicationforkids/DirectoryActivity.java
+++ MobileARApplicationForKids/app/src/main/java/com/example/martin/mobilearapplicationforkids/DirectoryActivity.java
@@ -7,18 +7,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class DirectoryActivity extends AppCompatActivity {
-    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directory);
-        button2 = (Button) findViewById( R.id.button2 );
-        button2.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mainAct = new Intent(DirectoryActivity.this, MainActivity.class);
-                startActivity( mainAct );
-            }
-        } );
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled( true );
+
     }
 }
--- MobileARApplicationForKids/app/src/main/java/com/example/martin/mobilearapplicationforkids/ThreeModelActivity.java
+++ MobileARApplicationForKids/app/src/main/java/com/example/martin/mobilearapplicationforkids/ThreeModelActivity.java
@@ -9,5 +9,7 @@ public class ThreeModelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_three_model );
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled( true );
     }
 }
